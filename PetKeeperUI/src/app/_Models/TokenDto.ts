d73c6c76f7eac2633b86abe70ce413b32d76b349
ibrahim21399 <ibrahimmohamed21399@gmail.com>
2022-07-08 01:40:07 +0200

    adding role string in token dto

--- PetKeeperUI/src/app/_Models/TokenDto.ts
+++ PetKeeperUI/src/app/_Models/TokenDto.ts
@@ -4,6 +4,7 @@ export class TokenDto {
     constructor(
         public token :string,
         public expiration :Date,
-        public currentUser : ApplicationUserDto
+        public currentUser : ApplicationUserDto,
+        public role :string
     ) {}
 }
