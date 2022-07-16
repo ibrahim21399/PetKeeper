ff859d7d9c7bc8494b870e34d812263eb013fae8
Sci Alex <sci.mohamedosama2018@alexu.edu.eg>
2022-07-16 22:36:36 +0200

    fix comment

---
+++ Application/Mapping/Comment/CommentProfilecs.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Dto.Client;
+using System;
+using Domain.Entites;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping.Comment
+{
+    public class CommentProfilecs : MappingProfileBase
+
+    {
+        public CommentProfilecs()
+        {
+            CreateMap<Comments,CreateCommentDto>().ReverseMap();
+        
+        
+        }
+    }
+}
